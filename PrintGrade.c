14cc99b21f7374330c9489b063529967b607d839
boonying04 <w_boonying@hotmail.com>
2017-04-24 16:23:25 +0700

    Changing 1 file

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void main()
 {
-  char Name[30][40] = {"Sam Matthews", "oum", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
+  char Name[30][40] = {"Sam Matthews", "Tum Yai", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
   int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306};
-  float GPA[30] = {3.73, 3.35, 3.65, 2.24, 1.32, 3.26};
+  float GPA[30] = {3.73, 0, 3.65, 2.24, 1.32, 3.26};
 
   int N = 6, i;
   int SearchID, found_index;
